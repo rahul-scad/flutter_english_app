d26cd8a9a4bb0a0790dd286f888a6f0f104b26c7
Rahul Kumar <rkece5897643@gmail.com>
2023-05-01 16:56:32 +0530

    first commit

--- lib/Grammarpage.dart
+++ lib/Grammarpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_english_app/GrammarCategory.dart';
 
 class GrammarPage extends StatelessWidget {
   const GrammarPage({Key? key}) : super(key: key);
@@ -24,6 +25,10 @@ class GrammarPage extends StatelessWidget {
               style: Theme.of(context).textTheme.bodyText1,
             ),
             Text(
+              'Pronoun - A word that takes the place of a noun',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Text(
               'Verb - An action or state of being',
               style: Theme.of(context).textTheme.bodyText1,
             ),
@@ -35,6 +40,45 @@ class GrammarPage extends StatelessWidget {
               'Adverb - A word that describes a verb, adjective, or other adverb',
               style: Theme.of(context).textTheme.bodyText1,
             ),
+            Text(
+              'Preposition - A word that shows the relationship between a noun or pronoun and other words in a sentence',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Text(
+              'Conjunction - A word that connects words, phrases, or clauses',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Text(
+              'Interjection - A word or phrase that expresses strong emotion',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Grammar Categories:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 8),
+            for (var category in grammarCategories)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    category.name,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  const SizedBox(height: 8),
+                  Wrap(
+                    spacing: 8,
+                    children: [
+                      for (var example in category.examples)
+                        Chip(
+                          label: Text(example),
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
           ],
         ),
       ),
