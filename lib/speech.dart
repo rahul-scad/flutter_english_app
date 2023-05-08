4b7a2f448d501167220233d5ad1ac37f626c0105
rahul-scad <rkece5897643@gmail.com>
2023-05-08 21:58:15 +0530

    final commit

--- lib/speech.dart
+++ lib/speech.dart
@@ -4,7 +4,7 @@ class Speak {
   FlutterTts flutterTts = FlutterTts();
 
   Future speak(String text) async {
-    await flutterTts.setLanguage("en-US");
+    await flutterTts.setLanguage("en-S");
     await flutterTts.setPitch(1);
     await flutterTts.speak(text);
   }
